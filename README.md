bddf298c43d27d557c3d801c7e84fd5a683a535b
Yang Lu <yang07ly@gmail.com>
2020-09-11 20:48:03 +1000

    changes style so that it implies with JS Standard

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Introduction
 
-This is a Command Line Interface (CLI) coded in NodeJS to extract a CSV file from some of the [BitMEX endpoints](https://testnet.bitmex.com/api/explorer/#!/Instrument/Instrument_get). It takes in a list of user inputs from command line, queries BitMEX, and stores the query results in a Common Separated Value (CSV) file. You may refer to `walletHistory.csv` or `instrument.csv` for a sample CSV query result.
+This is a Command Line Interface (CLI) coded in NodeJS to query for Bitcoin data and store it in a CSV file from some of the [BitMEX endpoints](https://testnet.bitmex.com/api/explorer/#!/Instrument/Instrument_get). It takes in a list of user inputs from the command line, queries BitMEX, and stores the query results in a Common Separated Value (CSV) file. You may refer to `walletHistory.csv` or `instrument.csv` for a sample CSV query result.
 
 ## Author
 
@@ -24,7 +24,7 @@ npm install
 
 ## Usage
 
-1. Create a `.env` file in project folder with the following paramters:
+1. Create a `.env` file in the project folder with the following paramters:
 ```
 API_KEY=[your BitMEX API key]
 API_SECRET=[your BitMEX Secret key]
@@ -72,6 +72,5 @@ Program closed.
 Now you can retrieve the CSV data from `instrument.csv`.
 
 ## Program Restrictions
-1. It only work for BitMEX.com RESTful API.
-2. It only work for BitMEX.com GET API.
-3. Only columns with number or string values are included in the CSV. Columns with values of other types such as a nested list or map are excluded.
+1. It only work for BitMEX.com GET API.
+2. Only columns with number or string values are included in the CSV. Columns with values of other types such as a nested list or map are excluded.
--- src/app.js
+++ src/app.js
@@ -1,134 +1,143 @@
 const fs = require('fs')
-const axios = require("axios")
+const axios = require('axios')
 const crypto = require('crypto')
 const readline = require('readline')
 const secrets = require('dotenv').config()
 
+// start point of the program
 const io = () => {
-	const rl = readline.createInterface({
-  		input: process.stdin,
-  		output: process.stdout
-	})
-	rl.question("Please enter data endpoint: ",
-		async (endpoint) => {
-		console.log("Please enter query parameters by line" + 
-			" (e.g. count: 50), hit enter in the new line to stop: ")
-		const params = []
-		for await (const line of rl) {
-			if (line.trim().length === 0) {
-				break
-			} else {
-				params.push(line.trim())
-			}
-		}
-		run(endpoint, params).then(response => {
-			console.log(response)
-			console.log("Program closed.")
-			rl.close()
-		}).catch(error => {
-			if (error.response) {
-				console.error(error.response.data)
-			} else {
-				console.error(error)
-			}
-			rl.close()
-		})
-	})
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.question('Please enter data endpoint: ',
+    async (endpoint) => {
+      console.log('Please enter query parameters by line' +
+        ' (e.g. count: 50), hit enter in the new line to stop: ')
+      const params = []
+      for await (const line of rl) {
+        if (line.trim().length === 0) {
+          break
+        } else {
+          params.push(line.trim())
+        }
+      }
+      run(endpoint, params).then(response => {
+        console.log(response)
+        console.log('Program closed.')
+        rl.close()
+      }).catch(error => {
+        if (error.response) {
+          console.error(error.response.data)
+        } else {
+          console.error(error)
+        }
+        rl.close()
+      })
+    })
 }
 
+// handles HTTP requests
 const run = (input, params) => {
-	if (!secrets) {
-		throw Error(".env file is missing.")
-	}
-	const BASE_URL = secrets.parsed['BASE_URL']
-	const API_SECRET = secrets.parsed['API_SECRET']
-	const API_KEY = secrets.parsed['API_KEY']
-	if (!BASE_URL || !API_SECRET || !API_KEY) {
-		throw Error("Please check your .env file.")
-	}
+  if (!secrets || !secrets.parsed) {
+    throw Error('.env file is missing.')
+  }
+  let BASE_URL = secrets.parsed.BASE_URL
+  if (BASE_URL[BASE_URL.length - 1] === '/') {
+    BASE_URL = BASE_URL.substring(0, BASE_URL.length - 1)
+  }
+  const API_SECRET = secrets.parsed.API_SECRET
+  const API_KEY = secrets.parsed.API_KEY
+  if (!BASE_URL || !API_SECRET || !API_KEY) {
+    throw Error('Please check your .env file.')
+  }
 
-	const output = getOutputFileName(input)
-	let path = '/api/v1/' + input
-	for (let i=0; i<params.length; i++) {
-		const param = params[i]
-		const key = param.substring(0, param.indexOf(":"))
-		const val = param.substring(param.indexOf(":")+1)
-		path += i == 0 ? "?" : "&"
-		path += key.trim() + "=" + encodeURI(val.trim())
-	}
-	const timestamp = getTimestamp(6000)
-	const signature = getSignature(
-		API_SECRET,
-		'GET',
-		path,
-		timestamp,
-		''
-	)
-	const instance = axios.create({
-		baseURL: BASE_URL,
-		headers: {
-			'api-expires': timestamp,
-			'api-key': API_KEY,
-			'api-signature': signature
-		}
-	})
-	console.log("Fetching data from " + BASE_URL + path + "...")
-	return instance.get(path)
-		.then(response => {
-			console.log("Successfully fetched data from " + 
-				BASE_URL + path + ".")
-			return processData(response.data, output)
-		})
+  const output = getOutputFileName(input)
+  let path = '/api/v1/' + input
+  for (let i = 0; i < params.length; i++) {
+    const param = params[i]
+    const key = param.substring(0, param.indexOf(':'))
+    const val = param.substring(param.indexOf(':') + 1)
+    path += i === 0 ? '?' : '&'
+    path += key.trim() + '=' + encodeURI(val.trim())
+  }
+  const timestamp = getTimestamp(6000)
+  const signature = getSignature(
+    API_SECRET,
+    'GET',
+    path,
+    timestamp,
+    ''
+  )
+  const instance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      'api-expires': timestamp,
+      'api-key': API_KEY,
+      'api-signature': signature
+    }
+  })
+  console.log('Fetching data from ' + BASE_URL + path + '...')
+  return instance.get(path)
+    .then(response => {
+      console.log('Successfully fetched data from ' +
+        BASE_URL + path + '.')
+      return processData(response.data, output)
+    })
 }
 
+// generates api-expires 
 const getTimestamp = num => {
-	return Math.round(Date.now() / 1000) + num
+  return Math.round(Date.now() / 1000) + num
 }
 
+// generates api-signature
 const getSignature = (secret, verb, path, timestamp, data) => {
-	const query = verb + path + timestamp + data
-	return crypto.createHmac("sha256", secret)
-		.update(query).digest("hex")
+  const query = verb + path + timestamp + data
+  return crypto.createHmac('sha256', secret)
+    .update(query).digest('hex')
 }
 
+// generates the filename of the output CSV
 const getOutputFileName = input => {
-	const params = input.split("/")
-	return params[params.length-1] + ".csv"
+  const params = input.split('/')
+  return params[params.length - 1] + '.csv'
 }
 
+// translates the query result into CSV content
 const processData = (data, output) => {
-	if (!Array.isArray(data)) {
-		data = [data]
-	}
-	console.log("Sample data", data[0])
-	const headers = []
-	for (const header in data[0]) {
-		if (typeof data[0][header] !== 'object') {
-			headers.push(header)
-		}
-	}
-	const rows = [headers.join(",")]
-	data.forEach(row => {
-		const content = []
-		for (let i=0; i<headers.length; i++) {
-			content.push(row[headers[i]])
-		}
-		rows.push(content.join(","))
-	})
-	const content = rows.join("\n")
-	console.log("Writing data to " + output + "...")
-	return writeCSV(output, content)
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
+  console.log('Sample data', data[0])
+  const headers = []
+  for (const header in data[0]) {
+    if (typeof data[0][header] !== 'object') {
+      headers.push(header)
+    }
+  }
+  const rows = [headers.join(',')]
+  data.forEach(row => {
+    const content = []
+    for (let i = 0; i < headers.length; i++) {
+      content.push(row[headers[i]])
+    }
+    rows.push(content.join(','))
+  })
+  const content = rows.join('\n')
+  console.log('Writing data to ' + output + '...')
+  return writeCSV(output, content)
 }
 
 const writeCSV = (path, data) => {
-	return new Promise((resolve, reject) => 
-		fs.writeFile(path, data, err => {
-			if (err) {
-				reject(err)
-			}
-			resolve("Successfully written data to " + path + ".")
-		})
-	)
+  return new Promise((resolve, reject) =>
+    fs.writeFile(path, data, err => {
+      if (err) {
+        reject(err)
+      }
+      resolve('Successfully written data to ' + path + '.')
+    })
+  )
 }
 
 io()
